29d7a3ed5df6d9ec1e09bebc9926356fa7c492b8
patrick <patrickwuke@163.com>
2019-08-09 13:58:31 +0800

    remove jodd

--- modules.jodd/src/main/java/io/hedwig/modules/jodd/db/BasicQuery.java
+++
@@ -1,8 +0,0 @@
-package io.hedwig.modules.jodd.db;
-
-public interface BasicQuery {
-    void createConnection();
-    public void basicQuery();
-    public void basicUpdate();
-    public void basicInsert();
-}
