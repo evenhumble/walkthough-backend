0b304a5a9c19dd6ed238c32045246ecbdb5c3e38
patrick <patrickwuke@163.com>
2019-08-10 10:41:10 +0800

    remove unused files

--- ee-in-action/src/main/java/io/hedwig/eeinaction/simplerpc/RpcTest.java
+++ ee-in-action/src/main/java/io/hedwig/eeinaction/simplerpc/RpcTest.java
@@ -5,18 +5,18 @@ import java.net.InetSocketAddress;
 
 public class RpcTest {
 
-    public static void main(String[] args) {
-        new Thread(() -> {
-            try {
-                RpcExporter.exporter("localhost",9090);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start();
+  public static void main(String[] args) {
+    new Thread(() -> {
+      try {
+        RpcExporter.exporter("localhost", 9090);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }).start();
 
-        RpcImporter<EchoService> importer = new RpcImporter<>();
-        EchoService echo = importer.importer(EchoServiceImpl.class,new InetSocketAddress("localhost",
-                                                                                         9090));
-        System.out.println(echo.echo("this is test"));
-    }
+    RpcImporter<EchoService> importer = new RpcImporter<>();
+    EchoService echo = importer.importer(EchoServiceImpl.class, new InetSocketAddress("localhost",
+                                                                                      9090));
+    System.out.println(echo.echo("this is test"));
+  }
 }
