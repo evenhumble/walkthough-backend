55891a80fa987c5a3fac9faf4b8ba0d189306541
patrick <patrickwuke@163.com>
2019-12-26 12:00:09 +0800

    remove lcd module

--- lcd/src/main/java/io/qkits/lcd/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author: patrick on 2019/12/22
- * @Description: LCD: low code development for productivities
- * Code Generation
- * Code Segment
- * AI or something
- */
-package io.qkits.lcd;
