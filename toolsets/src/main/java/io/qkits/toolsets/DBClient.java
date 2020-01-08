85c5c4a896a1217b64873d49ca7e3433dbf6e5a7
patrick <patrickwuke@163.com>
2020-01-08 14:18:08 +0800

    add toolsets

---
+++ toolsets/src/main/java/io/qkits/toolsets/DBClient.java
@@ -0,0 +1,12 @@
+package io.qkits.toolsets;
+
+/**
+ * @author: patrick on 2020/1/8
+ * @Description:
+ * 1. Query Data
+ * 2. Getting Data
+ * 3. Compare Data
+ */
+public class DBClient {
+
+}
---
+++ toolsets/src/main/java/io/qkits/toolsets/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author: patrick on 2020/1/8
+ * @Description:
+ */
+package io.qkits.toolsets;
