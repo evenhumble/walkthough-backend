a753f67c9b5a1f1b90fbe78a6991254175eaa18a
patrick <patrickwuke@163.com>
2020-01-29 21:59:48 +0800

    init try-chart module

--- try.chart/src/main/java/io/hedwig/dh/charts/TryChartApplication.java
+++ try.chart/src/main/java/io/hedwig/dh/charts/TryChartApplication.java
@@ -4,12 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class TryChartApplication extends WebMvcAutoConfiguration {
 
     public static void main(String[] args) {
