38979422582a19c03bf61734404809bda12eb4b2
patrick <patrickwuke@163.com>
2020-05-20 11:57:37 +0800

    add starter

---
+++ starters/poi-starter/src/main/java/io/qkits/poi/EasyPoiProperties.java
@@ -0,0 +1,9 @@
+package io.qkits.poi;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@ConfigurationProperties(prefix = "poi.base")
+public class EasyPoiProperties {
+
+}
